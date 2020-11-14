3a57f67d8273166701b589209b4add3f2ea033da
Ju Liu <ju@noredink.com>
2020-11-14 22:34:45 +0000

    Parse math operators with correct precedence

--- src/Expression.elm
+++ src/Expression.elm
@@ -1,12 +1,185 @@
 module Expression exposing (Expression(..), parser)
 
-import Parser exposing ((|.), (|=), Parser)
+import Parser exposing (..)
 
 
 type Expression
-    = Value
+    = Num Float
+    | Add Expression Expression
+    | Sub Expression Expression
+    | Mul Expression Expression
+    | Div Expression Expression
+    | Exp Expression Expression
 
 
 parser : Parser Expression
 parser =
-    Parser.succeed Value
+    expression
+
+
+digits : Parser Expression
+digits =
+    let
+        numParser =
+            number
+                { int = Just toFloat
+                , hex = Nothing
+                , octal = Nothing
+                , binary = Nothing
+                , float = Just identity
+                }
+    in
+    Parser.map Num <|
+        oneOf
+            [ succeed negate
+                |. symbol "-"
+                |= numParser
+            , numParser
+            ]
+
+
+{-| A term is a standalone chunk of math, like `4` or `(3 + 4)`. We use it as
+a building block in larger expressions.
+-}
+term : Parser Expression
+term =
+    oneOf
+        [ digits
+        , succeed identity
+            |. symbol "("
+            |. spaces
+            |= lazy (\_ -> expression)
+            |. spaces
+            |. symbol ")"
+        ]
+
+
+{-| Every expression starts with a term. After that, it may be done, or there
+may be more math.
+-}
+expression : Parser Expression
+expression =
+    term
+        |> andThen (expressionHelp [])
+
+
+{-| Once you have parsed a term, you can start looking for more operators.
+I am tracking everything as a list, that way I can be sure to follow the order
+of operations (PEMDAS) when building the final expression.
+In one case, I need an operator and another term. If that happens I keep
+looking for more. In the other case, I am done parsing, and I finalize the
+expression.
+-}
+expressionHelp : List ( Expression, Operator ) -> Expression -> Parser Expression
+expressionHelp revOps expr =
+    oneOf
+        [ succeed Tuple.pair
+            |. spaces
+            |= operator
+            |. spaces
+            |= term
+            |> andThen
+                (\( op, newExpr ) ->
+                    expressionHelp (( expr, op ) :: revOps) newExpr
+                )
+        , lazy (\_ -> succeed (finalize revOps expr))
+        ]
+
+
+type Operator
+    = AddOp
+    | SubOp
+    | MulOp
+    | DivOp
+    | ExpOp
+
+
+operator : Parser Operator
+operator =
+    oneOf
+        [ map (\_ -> AddOp) (symbol "+")
+        , map (\_ -> SubOp) (symbol "-")
+        , map (\_ -> MulOp) (symbol "*")
+        , map (\_ -> DivOp) (symbol "/")
+        , map (\_ -> ExpOp) (symbol "^")
+        ]
+
+
+precedence : Operator -> Int
+precedence op =
+    case op of
+        AddOp ->
+            0
+
+        SubOp ->
+            0
+
+        MulOp ->
+            1
+
+        DivOp ->
+            1
+
+        ExpOp ->
+            2
+
+
+opToExpr : Operator -> (Expression -> Expression -> Expression)
+opToExpr op =
+    case op of
+        AddOp ->
+            Add
+
+        SubOp ->
+            Sub
+
+        MulOp ->
+            Mul
+
+        DivOp ->
+            Div
+
+        ExpOp ->
+            Exp
+
+
+{-| This function is using the shunting yard algorithm.
+
+Imagine we have the expression `1 + 2 * 3`. We'd like to reduce it like this:
+
+> finalize [ ( Num 2, MulOp ), ( Num 1, AddOp ) ] (Num 3)
+> finalize [ ( Num 1, AddOp ) ] (Mul (Num 2) (Num 3))
+> finalize [] (Add (Num 1) (Mul (Num 2) (Num 3)))
+> Add (Num 1) (Mul (Num 2) (Num 3))
+
+If instead we have the expression `1 * 2 + 3`:
+
+> finalize [ ( Num 2, AddOp ), ( Num 1, MulOp ) ] (Num 3)
+> Add (finalize [ ( Num 1, MulOp ) ] (Num 2)) (Num 3)
+> Add (finalize [] (Mul (Num 1) (Num 2))) (Num 3)
+> Add (Mul (Num 1) (Num 2)) (Num 3)
+
+-}
+finalize : List ( Expression, Operator ) -> Expression -> Expression
+finalize revOps finalExpr =
+    case revOps of
+        [] ->
+            finalExpr
+
+        [ ( lastExpr, lastOp ) ] ->
+            let
+                expr =
+                    opToExpr lastOp
+            in
+            expr lastExpr finalExpr
+
+        ( firstExpr, firstOp ) :: (( _, secondOp ) as second) :: otherRevOps ->
+            let
+                expr =
+                    opToExpr firstOp
+            in
+            if precedence firstOp > precedence secondOp then
+                finalize (second :: otherRevOps) (expr firstExpr finalExpr)
+
+            else
+                expr (finalize (second :: otherRevOps) firstExpr) finalExpr
--- tests/ExpressionSpec.elm
+++ tests/ExpressionSpec.elm
@@ -1,21 +1,118 @@
 module ExpressionSpec exposing (spec)
 
 import Expect
-import Expression
+import Expression exposing (Expression(..))
 import Parser
 import Test exposing (..)
-
-
-
--- TESTS
 
 
 spec : Test
 spec =
     describe "Expression"
-        [ test "it does stuff" <|
-            \_ ->
-                "foobar"
-                    |> Parser.run Expression.parser
-                    |> Expect.equal (Ok Expression.Value)
+        [ describe "digits"
+            [ test "parses integers" <|
+                \_ ->
+                    parseEquals "10" (Num 10)
+            , test "parses floats" <|
+                \_ ->
+                    parseEquals "10.5" (Num 10.5)
+            , test "parses negative numbers" <|
+                \_ ->
+                    parseEquals "-10.0" (Num -10)
+            ]
+        , describe "simple math"
+            [ test "addition" <|
+                \_ ->
+                    parseEquals "10 + 20" (Add (Num 10) (Num 20))
+            , test "subtraction" <|
+                \_ ->
+                    parseEquals "10 - 5" (Sub (Num 10) (Num 5))
+            , test "multiplication" <|
+                \_ ->
+                    parseEquals "10 * 20.0" (Mul (Num 10) (Num 20))
+            , test "division" <|
+                \_ ->
+                    parseEquals "10 / 2" (Div (Num 10) (Num 2))
+            , test "exponents" <|
+                \_ ->
+                    parseEquals "10 ^ 2" (Exp (Num 10) (Num 2))
+            , test "parenthesis" <|
+                \_ ->
+                    parseEquals "(1 + 2) * 3"
+                        (Mul
+                            (Add (Num 1) (Num 2))
+                            (Num 3)
+                        )
+            ]
+        , describe "ordering"
+            [ test "add and sub" <|
+                \_ ->
+                    parseEquals "1 + 2 - 3"
+                        (Sub
+                            (Add (Num 1) (Num 2))
+                            (Num 3)
+                        )
+            , test "sub and add" <|
+                \_ ->
+                    parseEquals "1 - 2 + 3"
+                        (Add
+                            (Sub (Num 1) (Num 2))
+                            (Num 3)
+                        )
+            , test "add and mul" <|
+                \_ ->
+                    parseEquals "1 + 2 * 3"
+                        (Add
+                            (Num 1)
+                            (Mul (Num 2) (Num 3))
+                        )
+            , test "two muls and one add" <|
+                \_ ->
+                    parseEquals "1 * 2 + 3 * 4"
+                        (Add
+                            (Mul (Num 1) (Num 2))
+                            (Mul (Num 3) (Num 4))
+                        )
+            , test "add and div" <|
+                \_ ->
+                    parseEquals "1 + 2 / 3"
+                        (Add
+                            (Num 1)
+                            (Div (Num 2) (Num 3))
+                        )
+            , test "mul and div" <|
+                \_ ->
+                    parseEquals "1 / 2 * 3"
+                        (Mul
+                            (Div (Num 1) (Num 2))
+                            (Num 3)
+                        )
+            , test "PEMDAS" <|
+                \_ ->
+                    parseEquals
+                        "(1 + 2) ^ 3 * 4 / 5 + 6 - 7"
+                        (Sub
+                            (Add
+                                (Div
+                                    (Mul
+                                        (Exp
+                                            (Add (Num 1) (Num 2))
+                                            (Num 3)
+                                        )
+                                        (Num 4)
+                                    )
+                                    (Num 5)
+                                )
+                                (Num 6)
+                            )
+                            (Num 7)
+                        )
+            ]
         ]
+
+
+parseEquals : String -> Expression -> Expect.Expectation
+parseEquals str value =
+    str
+        |> Parser.run Expression.parser
+        |> Expect.equal (Ok value)
