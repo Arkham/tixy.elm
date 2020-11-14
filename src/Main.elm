49913a249a227c916cdd88f5c162dab6de0b396a
Ju Liu <ju@noredink.com>
2020-11-14 00:13:33 +0000

    Test out netlify deploy

--- src/Main.elm
+++ src/Main.elm
@@ -7,9 +7,8 @@ import Html.Attributes
 main : Html a
 main =
     Html.section []
-        [ Html.h1 [] [ Html.text "Minimal Elm + Parcel implementation, deployed to Netlify" ]
+        [ Html.h1 [] [ Html.text "tixy.elm" ]
         , Html.p []
-            [ Html.text "Find the source on "
-            , Html.a [ Html.Attributes.href "https://github.com/JoelQ/elm-netlify-parcel" ] [ Html.text "GitHub" ]
+            [ Html.text "Hello there!"
             ]
         ]
