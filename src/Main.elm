18935230debf6d8d13a3842c7ec2917aabac78f0
Ju Liu <ju@noredink.com>
2020-11-16 15:00:38 +0000

    Hook everything up

--- src/Main.elm
+++ src/Main.elm
@@ -2,23 +2,45 @@ module Main exposing (main)
 
 import Browser
 import Browser.Events as Events
+import Expression exposing (Expression)
 import Html exposing (Html, button, div, text)
 import Html.Attributes as Attr exposing (attribute, class)
 import Html.Events as Events
+import Parser
 import Time
 
 
 type alias Model =
     { time : Float
     , input : String
+    , expression : Maybe Expression
     }
 
 
 initialModel : Model
 initialModel =
+    let
+        initialInput =
+            "sin(t - sqrt((x-7.5)^2 + (y-6)^2))"
+    in
     { time = 0
-    , input = "sin(t - sqrt((x-7.5)^2 + (y-6)^2))"
+    , input = initialInput
+    , expression = parse initialInput
     }
+
+
+parse : String -> Maybe Expression
+parse str =
+    case Parser.run Expression.parser str of
+        Ok value ->
+            Just value
+
+        Err err ->
+            let
+                _ =
+                    Debug.log "err" err
+            in
+            Nothing
 
 
 type Msg
@@ -33,7 +55,7 @@ update msg model =
             ( { model | time = model.time + dt }, Cmd.none )
 
         ChangeInput str ->
-            ( { model | input = str }, Cmd.none )
+            ( { model | input = str, expression = parse str }, Cmd.none )
 
 
 main : Program () Model Msg
@@ -61,11 +83,13 @@ styleFromValue value =
         ]
 
 
-tixyFn t i x y =
-    sin (t - sqrt ((x - 7.5) ^ 2 + (y - 6) ^ 2))
+tixyFn exp t i x y =
+    exp
+        |> Maybe.map (Expression.evaluate { t = t, i = i, x = x, y = y })
+        |> Maybe.withDefault 0
 
 
-callTixyFn t intI =
+callTixyFn exp t intI =
     let
         i =
             toFloat intI
@@ -76,11 +100,11 @@ callTixyFn t intI =
         y =
             intI // 16
     in
-    clamp -1 1 <| tixyFn (t / 1000) i (toFloat x) (toFloat y)
+    clamp -1 1 <| tixyFn exp (t / 1000) i (toFloat x) (toFloat y)
 
 
-viewField t i =
-    div [ class "field", attribute "style" (styleFromValue (callTixyFn t i)) ] []
+viewField exp t i =
+    div [ class "field", attribute "style" (styleFromValue (callTixyFn exp t i)) ] []
 
 
 view : Model -> Html Msg
@@ -88,7 +112,7 @@ view model =
     div [ class "container" ]
         [ Html.h1 [] [ text "tixy.elm" ]
         , div [ class "map" ] <|
-            (List.range 0 255 |> List.map (viewField model.time))
+            (List.range 0 255 |> List.map (viewField model.expression model.time))
         , div [ class "editor" ]
             [ Html.span [] [ text "(t,i,x,y) =>" ]
             , Html.input
