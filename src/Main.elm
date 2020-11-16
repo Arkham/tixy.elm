17b1cd252c869275c79e3a1e3e81e57f7b9dd41b
Ju Liu <ju@noredink.com>
2020-11-16 14:51:53 +0000

    Adjust some styles

--- src/Main.elm
+++ src/Main.elm
@@ -3,21 +3,27 @@ module Main exposing (main)
 import Browser
 import Browser.Events as Events
 import Html exposing (Html, button, div, text)
-import Html.Attributes exposing (attribute, class)
+import Html.Attributes as Attr exposing (attribute, class)
+import Html.Events as Events
 import Time
 
 
 type alias Model =
-    { time : Float }
+    { time : Float
+    , input : String
+    }
 
 
 initialModel : Model
 initialModel =
-    { time = 0 }
+    { time = 0
+    , input = "sin(t - sqrt((x-7.5)^2 + (y-6)^2))"
+    }
 
 
 type Msg
     = Tick Float
+    | ChangeInput String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -25,6 +31,9 @@ update msg model =
     case msg of
         Tick dt ->
             ( { model | time = model.time + dt }, Cmd.none )
+
+        ChangeInput str ->
+            ( { model | input = str }, Cmd.none )
 
 
 main : Program () Model Msg
@@ -76,8 +85,20 @@ viewField t i =
 
 view : Model -> Html Msg
 view model =
-    div []
+    div [ class "container" ]
         [ Html.h1 [] [ text "tixy.elm" ]
         , div [ class "map" ] <|
             (List.range 0 255 |> List.map (viewField model.time))
+        , div [ class "editor" ]
+            [ Html.span [] [ text "(t,i,x,y) =>" ]
+            , Html.input
+                [ Attr.value model.input
+                , Events.onInput ChangeInput
+                , Attr.attribute "autocomplete" "off"
+                , Attr.attribute "autocapitalize" "off"
+                , Attr.attribute "spellcheck" "false"
+                , Attr.attribute "enterkeyhint" "go"
+                ]
+                []
+            ]
         ]
