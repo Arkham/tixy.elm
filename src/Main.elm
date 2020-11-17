cc47daf41ecd8649ada85f89517c2d6b5a02d93a
Ju Liu <ju@noredink.com>
2020-11-17 10:02:32 +0000

    Implement SVG rendering

--- src/Main.elm
+++ src/Main.elm
@@ -11,6 +11,8 @@ import Html.Events
 import Json.Decode
 import List.Zipper as Zipper exposing (Zipper)
 import Parser
+import Svg
+import Svg.Attributes as SvgAttr
 import Task
 import Time
 import Tutorial exposing (Tutorial)
@@ -45,7 +47,7 @@ init url key =
         allTutorials =
             Tutorial.all
 
-        { description, source } =
+        firstTutorial =
             Zipper.current allTutorials
 
         fromUrl =
@@ -57,12 +59,12 @@ init url key =
                     Nothing
 
         initialInput =
-            Maybe.withDefault source fromUrl
+            Maybe.withDefault firstTutorial.source fromUrl
     in
     { time = 0
     , input = initialInput
     , expression = parse initialInput
-    , help = description
+    , help = firstTutorial.description
     , tutorials = allTutorials
     , key = key
     }
@@ -115,6 +117,9 @@ update msg model =
 
         ChangeInput str ->
             let
+                -- we want to keep the last parseable expression so that the UI
+                -- doesn't flicker too much while the user is editing.
+                -- Only exception is when the whole input is cleared.
                 newExpression =
                     case ( String.trim str, parse str ) of
                         ( "", _ ) ->
@@ -149,12 +154,12 @@ update msg model =
             ( { model | help = "hit ENTER to update the url\nso you can share it" }, Cmd.none )
 
         InputKeyDown 13 ->
-            let
-                url =
-                    Url.Builder.absolute []
-                        [ Url.Builder.string "code" model.input ]
-            in
-            ( model, Navigation.replaceUrl model.key url )
+            ( model
+            , Navigation.replaceUrl model.key
+                (Url.Builder.absolute []
+                    [ Url.Builder.string "code" model.input ]
+                )
+            )
 
         InputKeyDown _ ->
             ( model, Cmd.none )
@@ -179,36 +184,12 @@ main =
         }
 
 
-styleFromValue : Float -> String
-styleFromValue value =
-    String.concat
-        [ "transform: scale("
-        , String.fromFloat value
-        , "); background-color: "
-        , if value < 0 then
-            "#f24"
-
-          else
-            "white"
-        , ";"
-        ]
-
-
-callTixyFn : Maybe Expression -> Float -> Int -> Float
-callTixyFn exp t intI =
+callTixyFn : Maybe Expression -> { t : Float, i : Int, x : Int, y : Int } -> Float
+callTixyFn exp { t, i, x, y } =
     let
-        i =
-            toFloat intI
-
-        x =
-            modBy 16 intI
-
-        y =
-            intI // 16
-
         tixy =
             { t = t / 1000
-            , i = i
+            , i = toFloat i
             , x = toFloat x
             , y = toFloat y
             }
@@ -221,11 +202,6 @@ callTixyFn exp t intI =
     clamp -1 1 result
 
 
-viewField : Maybe Expression -> Float -> Int -> Html msg
-viewField exp t i =
-    div [ class "field", attribute "style" (styleFromValue (callTixyFn exp t i)) ] []
-
-
 inputId : String
 inputId =
     "tixy-input"
@@ -235,8 +211,8 @@ view : Model -> Html Msg
 view model =
     div [ class "container" ]
         [ Html.h1 [] [ text "tixy.elm" ]
-        , div [ class "map", Html.Events.onClick NextTutorial ] <|
-            (List.range 0 255 |> List.map (viewField model.expression model.time))
+        , div [ class "map", Html.Events.onClick NextTutorial ]
+            [ viewSvg model.expression model.time ]
         , div [ class "editor" ]
             [ viewHelp model.help
             , Html.span [] [ text "(t,i,x,y) =>" ]
@@ -271,3 +247,82 @@ viewHelp str =
 onKeyDown : (Int -> msg) -> Html.Attribute msg
 onKeyDown tagger =
     Html.Events.on "keydown" (Json.Decode.map tagger Html.Events.keyCode)
+
+
+
+-- SVG
+
+
+size : Float
+size =
+    320
+
+
+gridSize : Int
+gridSize =
+    16
+
+
+cellSize : Float
+cellSize =
+    size / toFloat gridSize
+
+
+viewSvg : Maybe Expression -> Float -> Html msg
+viewSvg expr t =
+    let
+        size_ =
+            String.fromFloat size
+    in
+    Svg.svg
+        [ SvgAttr.width size_
+        , SvgAttr.height size_
+        , SvgAttr.viewBox ("0 0 " ++ size_ ++ " " ++ size_)
+        ]
+        (Svg.rect
+            [ SvgAttr.x "0"
+            , SvgAttr.y "0"
+            , SvgAttr.width size_
+            , SvgAttr.height size_
+            , SvgAttr.fill "#111"
+            ]
+            []
+            :: (List.range 0 255 |> List.map (renderCircle expr t))
+        )
+
+
+renderCircle expr t index =
+    let
+        col =
+            modBy gridSize index
+
+        row =
+            index // gridSize
+
+        ( x, y ) =
+            ( (toFloat col * cellSize) + cellSize / 2
+            , (toFloat row * cellSize) + cellSize / 2
+            )
+
+        result =
+            callTixyFn expr { t = t, i = index, x = col, y = row }
+
+        fillColor =
+            if result < 0 then
+                "#f24"
+
+            else
+                "white"
+
+        -- we want the maximum size of the circle to be slightly smaller
+        -- so the grid has a little bit of breathing room.
+        radius =
+            cellSize / 2 * 0.98 * abs result
+    in
+    Svg.circle
+        [ SvgAttr.cx (String.fromFloat x)
+        , SvgAttr.cy (String.fromFloat y)
+        , SvgAttr.r (String.fromFloat radius)
+        , SvgAttr.fill fillColor
+        ]
+        []
